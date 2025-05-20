16f30e5414562941317ede6387e89df318d87c3d
Nikerra <127662629+Nikerra@users.noreply.github.com>
2025-05-20 20:05:47 +0300

    Update usePost05.go

--- usePost05.go
+++ usePost05.go
@@ -1,1 +1,13 @@
+package main
 
+import (
+	"fmt"
+
+	post05 "github.com/Nikerra/goFirstStep"
+)
+
+func main() {
+	post05.Hostname = "localhost"
+	fmt.Println(post05.Port)
+	fmt.Println(post05.Hostname)
+}
